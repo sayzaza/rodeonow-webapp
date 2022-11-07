68b4b8b91d3771b1278ab37b3bbd691b0580cd5c
arturyahnychuk <arturyahnychuk@gmail.com>
2022-11-07 11:31:47 +0300

    Update notifications, grant-access pages, and video component
    - Add dropdown for video ellipse
    - Notifications redirect to appropriate tabs in grant access page
    - Receive tab number from the route in grant access page

--- vue-app/src/components/utilities/Video.vue
+++ vue-app/src/components/utilities/Video.vue
@@ -1,125 +1,166 @@
 <template>
-    <v-card v-if="videoUser || $store.state.selectedProfile" class="video-card">
-        <v-card-text class="d-flex justify-space-between py-1 px-2">
-            <div class="d-flex" style="max-width: 60%; overflow: hidden;">
-                <!--  -->
+  <v-card v-if="videoUser || $store.state.selectedProfile" class="video-card">
+    <v-card-text class="d-flex justify-space-between py-1 px-2">
+      <div class="d-flex" style="max-width: 60%; overflow: hidden">
+        <!--  -->
 
-                <router-link :to="{
-                    path: 'my-rodeo',
-                    query: {
-                        id: videoUser ? videoUser.id : $store.state.selectedProfile.id
-                    }
-                }">
-                    <v-avatar cover size="36" color="transparent">
-                        <v-img cover aspect-ratio="1" style="width: 100%"
-                            :src="videoUser ? videoUser.photo_url : ''">
-                        </v-img>
-                    </v-avatar>
-                </router-link>
-                
-                <div class="d-flex flex-column ml-2 ">
-                    <router-link 
-                    v-if="videoUser"
-                    :to="{
-                        path: 'my-rodeo',
-                        query: {
-                            id: videoUser ? videoUser.id : $store.state.selectedProfile.id
-                        }
-                    }">
-                        <span>{{ videoUser.first_name }} {{
-                        videoUser.last_name
-                        }}</span>
-                    </router-link>
-                    <span class="text-caption" v-else>{{ $store.state.first_name }} {{
-                        $store.state.selectedProfile.last_name
-                        }}</span>
-                    <span v-if="video.animal_name" class="text-caption">{{ video.animal_name }} <span
-                            v-if="video.animal_brand">({{
-                            video.animal_brand }})</span></span>
-                    <span>{{ video.title }}</span>
-                </div>
-            </div>
-            <!-- 
+        <router-link
+          :to="{
+            path: 'my-rodeo',
+            query: {
+              id: videoUser ? videoUser.id : $store.state.selectedProfile.id,
+            },
+          }"
+        >
+          <v-avatar cover size="36" color="transparent">
+            <v-img
+              cover
+              aspect-ratio="1"
+              style="width: 100%"
+              :src="videoUser ? videoUser.photo_url : ''"
+            >
+            </v-img>
+          </v-avatar>
+        </router-link>
+
+        <div class="d-flex flex-column ml-2">
+          <router-link
+            v-if="videoUser"
+            :to="{
+              path: 'my-rodeo',
+              query: {
+                id: videoUser ? videoUser.id : $store.state.selectedProfile.id,
+              },
+            }"
+          >
+            <span>{{ videoUser.first_name }} {{ videoUser.last_name }}</span>
+          </router-link>
+          <span class="text-caption" v-else
+            >{{ $store.state.first_name }}
+            {{ $store.state.selectedProfile.last_name }}</span
+          >
+          <span v-if="video.animal_name" class="text-caption"
+            >{{ video.animal_name }}
+            <span v-if="video.animal_brand">({{ video.animal_brand }})</span></span
+          >
+          <span>{{ video.title }}</span>
+        </div>
+      </div>
+      <!-- 
             <div class="text--disabled mt-auto" :title="video.location">
                 {{ video.location.slice(0,20) }}{{ video.location.length > 20 ? '...' : '' }}
             </div> -->
 
-            <div class="d-flex flex-column text-end mr-1">
-                <div class="d-flex align-center">
-                    <span class="mr-1">{{ getDate() }}</span>
-                    <v-btn icon variant="flat" size="small">
-                        <v-icon>fas fa-ellipsis</v-icon>
-                    </v-btn>
-                </div>
+      <div class="d-flex flex-column text-end mr-1">
+        <div class="d-flex align-center">
+          <span class="mr-1">{{ getDate() }}</span>
+          <v-menu v-model="menu" :close-on-content-click="false" location="end">
+            <template v-slot:activator="{ props }">
+              <v-btn icon variant="flat" size="small" v-bind="props">
+                <v-icon>fas fa-ellipsis</v-icon>
+              </v-btn>
+            </template>
+            <v-card min-width="300">
+              <v-list>
+                <v-list-item class="pa-0" v-if="props.video.user_id === $store.state.selectedProfile.id">
+                    <v-btn variant="flat" block class="text-black">Edit</v-btn>
+                </v-list-item>
+                <v-list-item class="pa-0" v-if="props.video.user_id === $store.state.selectedProfile.id">
+                    <v-btn variant="flat" block class="text-black">Share</v-btn>
+                </v-list-item>
+                <v-list-item class="pa-0" v-if="props.video.user_id === $store.state.selectedProfile.id">
+                    <v-btn variant="flat" block class="text-black">Download</v-btn>
+                </v-list-item>
+                <v-list-item class="pa-0" v-if="props.video.user_id === $store.state.selectedProfile.id">
+                    <v-btn variant="flat" block class="text-red">Delete</v-btn>
+                </v-list-item>
+                <v-list-item class="pa-0">
+                    <v-btn variant="flat" block class="text-black">Report</v-btn>
+                </v-list-item>
+                
+              </v-list>
+            </v-card>
+          </v-menu>
+        </div>
 
-                <div :title="video.location">
-                    {{ video.location.slice(0,20) }}{{ video.location.length > 20 ? '...' : '' }}
-                </div>
-            </div>
-        </v-card-text>
+        <div :title="video.location">
+          {{ video.location.slice(0, 20) }}{{ video.location.length > 20 ? "..." : "" }}
+        </div>
+      </div>
+    </v-card-text>
 
-        <v-img :key="videoUser" @click="playVideo" min-width="100%" :src="video.thumbnail_url"
-            class="d-flex align-center" aspect-ratio="1.7" cover>
-            <img src="/assets/icons/glyph/glyphs/play.circle.png" class="mx-auto play-icon">
-        </v-img>
-    </v-card>
+    <v-img
+      :key="videoUser"
+      @click="playVideo"
+      min-width="100%"
+      :src="video.thumbnail_url"
+      class="d-flex align-center"
+      aspect-ratio="1.7"
+      cover
+    >
+      <img src="/assets/icons/glyph/glyphs/play.circle.png" class="mx-auto play-icon" />
+    </v-img>
+  </v-card>
 </template>
 
-<script >
-import store from '@/store'
+<script>
+import store from "@/store";
+import { ref } from "vue";
 export default {
-    props: ['video', 'videoUser'],
-    setup(props) {
-        function playVideo() {
-            store.commit('SET_MODAL_VIDEO', props.video)
-            store.commit('VIDEO_PLAYER_MODAL', true)
-        }
-        function getDate() {
-            let endString = 'N/A';
-            if (props.video.event_date.toDate) {
-                endString = props.video.event_date.toDate().toDateString()
-            }
-            else {
-                try {
-                    endString = (new Date(props.video.event_date * 1000)).toDateString()
-                    // console.log(">>", props.video, props.video.event_date, endString)
-                } catch (error) {
-                    endString = props.video.event_date
-                }
-            }
-            if (endString !== 'N/A' && endString != props.video.event_date) {
-                endString = endString.split(' ').slice(1, 4)
-                endString = `${endString[0]} ${endString[1]}, ${endString[2]}`
-            }
-            return endString
-        }
-        return {
-            playVideo,
-            getDate
-        }
+  props: ["video", "videoUser"],
+  setup(props) {
+    const menu = ref(null)
+    function playVideo() {
+      store.commit("SET_MODAL_VIDEO", props.video);
+      store.commit("VIDEO_PLAYER_MODAL", true);
     }
-}
+    function getDate() {
+      let endString = "N/A";
+      if (props.video.event_date.toDate) {
+        endString = props.video.event_date.toDate().toDateString();
+      } else {
+        try {
+          endString = new Date(props.video.event_date * 1000).toDateString();
+          // console.log(">>", props.video, props.video.event_date, endString)
+        } catch (error) {
+          endString = props.video.event_date;
+        }
+      }
+      if (endString !== "N/A" && endString != props.video.event_date) {
+        endString = endString.split(" ").slice(1, 4);
+        endString = `${endString[0]} ${endString[1]}, ${endString[2]}`;
+      }
+      return endString;
+    }
+    return {
+      playVideo,
+      getDate,
+      menu,
+      props
+    };
+  },
+};
 </script>
 
 <style>
 .video-card {
-    overflow: hidden;
+  overflow: hidden;
 }
 
 .video-card:hover img {
-    cursor: pointer;
+  cursor: pointer;
 }
 .video-card:hover .play-icon {
-    transform: scale(1.1);
+  transform: scale(1.1);
 }
 
 .play-icon {
-    display: block;
-    filter: invert();
+  display: block;
+  filter: invert();
 }
 
 .v-theme--light a {
-    text-decoration: none;
-    color: black;
+  text-decoration: none;
+  color: black;
 }
 </style>
