2dba48710a10cd822c2c5e6679ead91529446e32
arturyahnychuk <arturyahnychuk@gmail.com>
2022-11-30 15:03:29 +0300

    update firebase config

--- vue-app/src/plugins/firebase.js
+++ vue-app/src/plugins/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwL8JLKEuXiBKRKyYy0QvmrmcT0xpiK7w",
-  authDomain: "rodeotestenv.firebaseapp.com",
-  databaseURL: "https://rodeotestenv-default-rtdb.firebaseio.com",
-  projectId: "rodeotestenv",
-  storageBucket: "rodeotestenv.appspot.com",
-  messagingSenderId: "843094037691",
-  appId: "1:843094037691:web:edc4f47d74e1454c05859e",
-  measurementId: "G-HST8L8SR9Y"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 initializeApp(firebaseConfig);
