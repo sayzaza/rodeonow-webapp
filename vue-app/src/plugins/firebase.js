f96f96ad16eb3d03d92c523d96eaff6be541dfc7
arturyahnychuk <arturyahnychuk@gmail.com>
2022-12-03 08:50:17 +0300

    update to use env file

--- vue-app/src/plugins/firebase.js
+++ vue-app/src/plugins/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwL8JLKEuXiBKRKyYy0QvmrmcT0xpiK7w",
-  authDomain: "rodeotestenv.firebaseapp.com",
-  databaseURL: "https://rodeotestenv-default-rtdb.firebaseio.com",
-  projectId: "rodeotestenv",
-  storageBucket: "rodeotestenv.appspot.com",
-  messagingSenderId: "843094037691",
-  appId: "1:843094037691:web:edc4f47d74e1454c05859e",
-  measurementId: "G-HST8L8SR9Y"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
