fe1e9979379994fea501574e79b678982479fc98
arturyahnychuk <arturyahnychuk@gmail.com>
2022-12-15 10:08:24 -0400

    4) Edit animal

--- vue-app/src/views/portal/MyRodeo.vue
+++ vue-app/src/views/portal/MyRodeo.vue
@@ -11,7 +11,7 @@
             </v-avatar>
             <div class="d-flex flex-column text-center">
                 <h3 class="h4">{{ idUserProfile.first_name }} {{ idUserProfile.last_name
-                    }}</h3>
+                }}</h3>
                 <span class="text--disabled">{{ idUserProfile.location }}</span>
             </div>
         </div>
@@ -42,8 +42,8 @@
                     placeholder="Start typing to Search Animals" return-object class="py-0 mr-3"
                     style="max-width: 440px;"></v-text-field>
                 <div class="ml-auto d-flex align-center">
-                    <v-btn @click="$router.push({ path: '/animals/new' })" v-if="idUserProfile.account_type == 1" icon size="small" variant="text"
-                        class="d-flex items-center justify-center mr-2">
+                    <v-btn @click="$router.push({ path: '/animals/new' })" v-if="idUserProfile.account_type == 1" icon
+                        size="small" variant="text" class="d-flex items-center justify-center mr-2">
                         <img class="mt-1" :src="require('@/assets/icons/glyph/glyphs/plus.circle.png')" />
                     </v-btn>
 
@@ -63,27 +63,37 @@
                 </div>
             </div>
 
-            <v-card @click="$router.push({
-                path: '/animals/edit',
-                params: {
-                    id: animal.id
-                }
-            })" flat class="d-flex pa-2" v-for="animal in filteredAnimals" :key="animal.animalID">
+            <AnimalCard v-for="animal in filteredAnimals" :animal="animal" 
+            :key="animal.id"/>
+            <!-- <v-card flat class="d-flex pa-2" v-for="animal in filteredAnimals" :key="animal.animalID">
                 <span class="mr-3 text--disabled" style="min-width: 100px">{{ animal.brand }}</span>
                 <span v-if="animal.name && animal.name.length > 0">{{ animal.name }}</span>
                 <span v-else class="text--disabled">Unnamed</span>
-                <span class="ml-auto">
-                    <v-icon size="13">fas fa-ellipsis</v-icon>
-                </span>
-            </v-card>
+                <v-menu v-model="animal_menu" :close-on-content-click="false" location="end">
+                    <template v-slot:activator="{ props }">
+                        <v-btn fab icon size="small" variant="text" class="ml-auto" v-bind="props">
+                            <v-icon>fas fa-ellipsis</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-btn @click="$router.push({ path: '/animals/edit', params: { id: animal.id } })" variant="text" block class="text-black">
+                            Edit
+                        </v-btn>
+                        <v-divider></v-divider>
+                        <v-btn variant="text" block class="text-black">
+                            Delete
+                        </v-btn>
+                    </v-list>
+                </v-menu>
+            </v-card> -->
             <span v-if="!animals || animals.length == 0" class="font-italic">No animals to show</span>
         </div>
 
         <div v-if="idUserProfile && showVideo" style="width: 100%; max-width: 900px;"
             class="d-flex flex-wrap mx-auto justify-space-between">
             <div style="width: 100%" class="d-flex align-center mb-6">
-                <v-btn v-if="idUserProfile.account_type == 1" @click="showVideo = !showVideo" icon
-                    variant="text" class="d-flex items-center justify-center mr-1">
+                <v-btn v-if="idUserProfile.account_type == 1" @click="showVideo = !showVideo" icon variant="text"
+                    class="d-flex items-center justify-center mr-1">
                     <img style="width: 30px;" class="mt-1"
                         :src="require('@/assets/icons/glyph/glyphs/list.dash.png')" />
                 </v-btn>
@@ -93,8 +103,7 @@
             </div>
             <span v-if="!videos || videos.length == 0" class="font-italic">No videos to show</span>
             <VideoVue style="width: 100%" :class="(index + 1) % 1 !== 0 ? 'ml-auto' : ''" class="mb-5"
-                v-for="(video, index) in videos" 
-                :videoUser="videoUsers[index] ? videoUsers[index] : null"
+                v-for="(video, index) in videos" :videoUser="videoUsers[index] ? videoUsers[index] : null"
                 :video="video" :key="index" />
 
             <!-- <div style="width: 32%" v-if="videos.length % 3 !== 0"></div> -->
@@ -111,14 +120,16 @@ import VideoVue from '@/components/utilities/Video.vue'
 import store from '@/store'
 import { useRoute } from 'vue-router'
 import { getProfileImageById } from '@/services/profiles'
+import AnimalCard from '@/components/utilities/animalCard.vue'
 const db = getFirestore()
 export default {
-    components: { VideoVue },
+    components: { VideoVue, AnimalCard },
     setup() {
         const route = useRoute()
         const search = ref('')
         const select_animal = ref(2)
         const videoUsers = ref([])
+        const animal_menu = ref(false)
         const videos = computed(() => {
             let localVideos = store.state.videos || []
             localVideos.sort((a, b) => {
@@ -131,7 +142,7 @@ export default {
                         || video.animal_brand && video.animal_brand.toLowerCase().includes(search.value.toLowerCase())
                         || video.animal_name && video.animal_name.toLowerCase().includes(search.value.toLowerCase())
                 })
-            } catch (error) { console.error(error)}
+            } catch (error) { console.error(error) }
             return localVideos || []
         })
         watch(videos, async (newVs) => {
@@ -150,7 +161,7 @@ export default {
             return store.state.userProfile
         })
         watch(userProfile, (v) => {
-            if(v) initialSetup()
+            if (v) initialSetup()
         })
         const showVideo = ref(false)
         const animals = computed(() => {
@@ -161,8 +172,8 @@ export default {
                         || animal.brand.toLowerCase().includes(search.value.toLowerCase())
                         || animal.contractor_name.toLowerCase().includes(search.value.toLowerCase())
                         || animal.contractor_name.toLowerCase().includes(search.value.toLowerCase())
-                })    
-            } catch (error) {}
+                })
+            } catch (error) { }
             localAnimals.sort((a, b) => {
                 if (a.name < b.name) { return 1; }
                 if (a.name > b.name) { return -1; }
@@ -175,14 +186,14 @@ export default {
             return localAnimals
         })
         const coverPhoto = computed(() => {
-            if (!idUserProfile.value) return 
-            return idUserProfile.value.account_type !== 2 ? 
+            if (!idUserProfile.value) return
+            return idUserProfile.value.account_type !== 2 ?
                 idUserProfile.value.account_type !== 3 ? require('@/assets/images/contractors.jpg') : require('@/assets/images/bareback.jpg')
-            : require('@/assets/images/contestants.jpg')
+                : require('@/assets/images/contestants.jpg')
         })
         const filteredAnimals = computed(() => {
             let localAnimals = animals.value
-            if (select_animal.value !== 2) localAnimals = localAnimals.filter(animal => animal.animal_type == select_animal.value+1)
+            if (select_animal.value !== 2) localAnimals = localAnimals.filter(animal => animal.animal_type == select_animal.value + 1)
             return localAnimals
         })
         const idUserProfile = ref(null)
@@ -226,6 +237,7 @@ export default {
             animals,
             showVideo,
             idUserProfile,
+            animal_menu,
             videoUsers,
             initialSetup
         }
@@ -234,6 +246,5 @@ export default {
 </script>
 
 <style>
-
 
 </style>
